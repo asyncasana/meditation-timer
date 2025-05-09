a0cea82e20ed9814431b61e25643c3b8fce1cb2a
asyncasana <dash.boyeva@gmail.com>
2025-05-09 14:31:28 +0100

    Update metadata name and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-indigo-900 to-slate-900 text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            <span className="text-amber-200">Zen</span>Timer
+            <span className="text-amber-200">Sanu</span>Timer
           </h1>
           
           <p className="text-xl text-center max-w-md">
